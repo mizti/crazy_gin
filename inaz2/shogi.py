e37713eb6975f4911da94cb2b3cdf82f1957eb67
inaz2 <inaz2@twitter>
2013-01-26 01:10:51 +0900

    失敗数を集計するようにし、メモ化を加えたら解けた

--- inaz2/shogi.py
+++ inaz2/shogi.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+import sys
+
+def memoize(f):
+    memo = {}
+    def __f(*args, **kwargs):
+        key = repr((args, kwargs))
+        if not key in memo:
+            memo[key] = f(*args, **kwargs)
+        return memo[key]
+    return __f
+
 class Shogi():
     # 0 = empty, 1 = friend, 2 = king
     board = [
@@ -20,12 +31,14 @@ class Shogi():
         (1, -1),
         (1, 1),
     ]
+    max_depth = 20
 
+    @memoize
     def step(self, xy, flags, depth):
-        if depth >= 20:
-            return [flags['got_king'] and not flags['is_fallen'] and not flags['killed_friend']]
+        if depth >= Shogi.max_depth:
+            return int(not (flags['got_king'] and not flags['is_fallen'] and not flags['killed_friend']))
 
-        results = []
+        num_failures = 0
         for move in Shogi.moves:
             flags2 = dict(flags)
             xy2 = (xy[0] + move[0], xy[1] + move[1])
@@ -34,15 +47,19 @@ class Shogi():
             if not is_now_fallen:
                 flags2['got_king'] = flags2['got_king'] or (Shogi.board[xy2[1]*9+xy2[0]] == 2)
                 flags2['killed_friend'] = flags2['killed_friend'] or (Shogi.board[xy2[1]*9+xy2[0]] == 1)
-            sub_results = self.step(xy2, flags2, depth+1)
-            results.extend(sub_results)
-        print xy, flags, depth
-        return results
+            sub_num_failures = self.step(xy2, flags2, depth+1)
+            num_failures = num_failures + sub_num_failures
+
+        print >>sys.stderr, '[DEBUG] depth = %d, \txy = %s, \tnum_failures = %d' % (depth, xy, num_failures)
+        return num_failures
 
     def run(self):
         initial_flags = {'got_king': False, 'is_fallen': False, 'killed_friend': False}
-        results = self.step((5, 5), initial_flags, 0)
-        print float(sum(results))/len(results)
+        num_failures = self.step((5, 5), initial_flags, 0)
+        num_all_candidates = 5 ** Shogi.max_depth
+        num_successes = num_all_candidates - num_failures
+        result = num_successes/float(num_all_candidates)
+        print '%d / %d = %f' % (num_successes, num_all_candidates, result)
 
 if __name__ == '__main__':
     Shogi().run()
